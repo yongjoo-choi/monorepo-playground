830ed9b58bf8bd3c37e61ed29be07ae47784feeb
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-05-04 18:07:08 +0900

    add dashboard pagge

---
+++ pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Dashboard: NextPage = () => {
+  return <div className={styles.container}>dashboard</div>;
+};
+
+export default Dashboard;
