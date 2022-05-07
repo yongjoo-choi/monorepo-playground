f5f358959f3c84416ec4bb29df353801821ba696
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-05-07 14:29:26 +0900

    add chakra-ui

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
+import { Box } from "@chakra-ui/react";
+
 import styles from "../styles/Home.module.css";
 
 const Dashboard: NextPage = () => {
-  return <div className={styles.container}>dashboard</div>;
+  return <Box>dashboard</Box>;
 };
 
 export default Dashboard;
