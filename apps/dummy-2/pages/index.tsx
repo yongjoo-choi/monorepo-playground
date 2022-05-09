c6677bef213281feedfd0774f43bb4d6b5ac25b4
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-05-09 15:00:02 +0900

    test turbo remote cache

--- apps/dummy-2/pages/index.tsx
+++ apps/dummy-2/pages/index.tsx
@@ -1,16 +1,16 @@
 import { Box } from "@chakra-ui/react";
 import { Button, Header } from "@zoomable/ui";
 
-export default function DummyOne() {
+export default function DummyTwo() {
   return (
     <Box>
       <Header
-        user={{ name: "dummy 1" }}
+        user={{ name: "dummy 2" }}
         onLogin={() => console.log("log in")}
         onLogout={() => console.log("log out")}
         onCreateAccount={() => console.log("create account")}
       />
-      <Button label="Button" />
+      <Button label="Button!!!" />
     </Box>
   );
 }
