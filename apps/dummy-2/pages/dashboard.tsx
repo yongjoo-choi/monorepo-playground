c61027993123e1ec464784ad55e65a207a6b9d18
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-05-09 15:28:42 +0900

    test remote cache

--- apps/dummy-2/pages/dashboard.tsx
+++ apps/dummy-2/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { Box } from "@chakra-ui/react";
 
 const Dashboard: NextPage = () => {
-  return <Box>dashboard remote cache 222</Box>;
+  return <Box>dashboard remote cache 333</Box>;
 };
 
 export default Dashboard;
